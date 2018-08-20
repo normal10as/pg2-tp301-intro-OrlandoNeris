4189fd425cc903494ac7746de048738d7995767a
OrlandoNeris <orlandoneris24@gmail.com>
2018-08-20 02:55:29 -0300

    tt

---
+++ Mascotas/Entidades/Loro.vb
@@ -0,0 +1,59 @@
+﻿Public Class Loro
+
+    Private _memoria As Queue(Of String)
+
+    Private _edad As Short
+    Public Property Edad As Short
+        Get
+            Return _edad
+        End Get
+        Set(value As Short)
+            _edad = value
+        End Set
+    End Property
+
+    Private _fechaNac As Date
+    Public Property FechaNac As Date
+        Get
+            Return _fechaNac
+        End Get
+        Set(value As Date)
+            _fechaNac = value
+        End Set
+    End Property
+
+    Private _nombre As String
+    Public Property Nombre As String
+        Get
+            Return _nombre
+        End Get
+        Set(value As String)
+            _nombre = value
+        End Set
+    End Property
+
+    Sub New()
+        _memoria = New Queue(Of String)
+        FechaNac = Today
+        Nombre = "sin nombre"
+        Edad = CalcularEdad(FechaNac)
+    End Sub
+
+    Public Function CalcularEdad(fechanac As Date) As UShort
+        Return Today.Year - fechanac.Year
+    End Function
+
+    Public Function Hablar() As String
+
+        Return _memoria.Dequeue
+    End Function
+
+    Public Sub Escuchar(palabra As String)
+        _memoria.Enqueue(palabra)
+    End Sub
+
+    Public Overrides Function ToString() As String
+        Return Me.Nombre
+    End Function
+
+End Class
---
+++ Mascotas/EntidadesTest/Loro Test.vb
@@ -0,0 +1,34 @@
+﻿Imports Entidades
+
+Module Loro_Test
+
+    Sub main()
+
+        Dim loro1 = New Loro
+        Imprimir(loro1)
+
+        loro1.Nombre = "Mariano"
+        loro1.FechaNac = # 08/02/1993#
+        loro1.Edad = loro1.CalcularEdad(loro1.FechaNac)
+        loro1.Escuchar("Hola")
+        loro1.Escuchar("Como")
+        loro1.Escuchar("Estan")
+
+        Imprimir(loro1)
+
+        Console.ReadKey()
+
+    End Sub
+
+    Public Sub Imprimir(loro As Loro)
+
+        Console.WriteLine("Nombre: " & loro.Nombre)
+        Console.WriteLine("Fecha de Nacimiento: " & loro.FechaNac)
+        Console.WriteLine("Edad: " & loro.Edad)
+        Console.WriteLine("To String: " & loro.ToString)
+        Console.WriteLine()
+
+    End Sub
+
+
+End Module
